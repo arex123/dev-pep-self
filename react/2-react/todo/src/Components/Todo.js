ef9849216dde4dfbe6a0eefb4221c5880022e0f4
arex123 <ad47kumar@gmail.com>
2021-11-26 18:49:05 +0530

    excel clone project started

---
+++ react/2-react/todo/src/Components/Todo.js
@@ -0,0 +1,14 @@
+//rcc + tab 
+import React, { Component } from 'react'
+
+class Todo extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+
+export default Todo;
