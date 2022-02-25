325c2aacc8a68923f964afd41ddf31324d64d870
arex123 <ad47kumar@gmail.com>
2022-02-25 23:13:18 +0530

    compare version numbers

---
+++ javascript module/js intro/array.js
@@ -0,0 +1,6 @@
+let arr =[1,0,[2,3,[88,9],4]];
+
+let res = arr.flat(Infinity);
+
+console.log(arr.length);
+console.log(res.length);
--- javascript module/js intro/intro.js
+++ javascript module/js intro/intro.js
@@ -1,2 +1,32 @@
+/*
 //if we want to print backslash also so this is the format \"\\n\"
 console.log(" hello guys \"\\n\" my name is aditya");
+
+//concatination
+var a = "hello";
+var b = "javascript"
+
+console.log(a+" "+b);
+
+*/
+
+/*template litrals are the backtick quoted string, they can embed variable or values inside them
+
+`half of 100 is ${100 / 2}`
+When you write something inside ${} in a template literal, its result will be computed, converted to a string, and included at that position. The example produces “half of 100 is 50”.
+
+
+console.log(`half of 100 is ${100/2}`);
+*/
+
+// boolean
+console.log(5>2); //true
+
+// but
+console.log(NaN==NaN);  //false
+
+console.log("5"*5); //nice, it will print 25
+
+console.log(8*null); // 0
+
+console.log("five"*5); //not a number NaN
