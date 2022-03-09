95f06909a6a29ffdee7d4b8b98e1c04e079deb4b
arex123 <ad47kumar@gmail.com>
2022-03-09 12:40:04 +0530

    browser automation using puppeteer

--- javascript module/promise/BrowserAutom.js
+++ javascript module/promise/BrowserAutom.js
@@ -1,3 +1,6 @@
+//for more info about puppeteer go to https://pptr.dev
+//https://flaviocopes.com/puppeteer/
+//https://nitayneeman.com/posts/getting-to-know-puppeteer-using-practical-examples/
 const puppeteer = require("puppeteer");
 console.log("before");
 let cPage;
@@ -27,6 +30,17 @@ browserOpenPromise
     let enterWillBePressed = cPage.keyboard.press("Enter");
     return enterWillBePressed;
 }).then(function(){
+    //now we want to go to the pepcoding website after searching "pepcoding on google"
+    //so now we will click on first link which is main pepcoding wesite, so for that we will
+    //wait for its css selector after getting selector next step will be clicking on it
+    let elementWaitPromise = cPage.waitForSelector("h3.LC20lb.MBeuO.DKV0Md",{visisble:true});
+    return elementWaitPromise;
+}).then(function(){
+    //now we will click on first suggested website link for pepcoding, as we already got the selector for the website
+    let keyWillBeSendPromise = cPage.click("h3.LC20lb.MBeuO.DKV0Md");
+    return keyWillBeSendPromise;
+})
+.then(function(){
     console.log("all work done");
 }).catch(function(err){
     console.log(err);
